20406df7ce4f198c92747b02ffea8cc980d360ab
Eliasyoussef47 <d-s-a00@hotmail.com>
2018-02-01 11:35:29 +0100

    bijna klaar

--- game2.js
+++ game2.js
@@ -1,13 +1,30 @@
-/* global words*/
-var chosenWord = words[Math.floor(Math.random() * words.length)];
-console.log(chosenWord);
-var splitChosenWord;
+// JavaScript Document
+/* global words, CryptoJS*/
+var key = CryptoJS.enc.Hex.parse((Math.floor((Math.random() * 999999999999999999))).toString(16));
+var iv = CryptoJS.enc.Hex.parse((Math.floor((Math.random() * 999999999999999999))).toString(16));
+
+var eSplitChosenWord = words[Math.floor(Math.random() * words.length)].split("");
+
+var eLetter1 = eSplitChosenWord[0];
+var eLetter2 = CryptoJS.AES.encrypt(eSplitChosenWord[1], key, { iv: iv });
+var eLetter3 = CryptoJS.AES.encrypt(eSplitChosenWord[2], key, { iv: iv });
+var eLetter4 = CryptoJS.AES.encrypt(eSplitChosenWord[3], key, { iv: iv });
+var eLetter5 = CryptoJS.AES.encrypt(eSplitChosenWord[4], key, { iv: iv });
+
+eSplitChosenWord = [eLetter1, eLetter2, eLetter3, eLetter4, eLetter5];
+var eChosenWord = eSplitChosenWord.join("");
+
+
+
+
+
+
+
 var attempt;
 var splitAttempt;
 var table = document.getElementsByTagName("table")[0];
 var tryRow1Letter1Input = document.getElementById("tryRow1Letter1Input");
 var tryRow1Letter2Input = document.getElementById("tryRow1Letter2Input");
-tryRow1Letter2Input.focus();
 var tryRow1Letter3Input = document.getElementById("tryRow1Letter3Input");
 var tryRow1Letter4Input = document.getElementById("tryRow1Letter4Input");
 var tryRow1Letter5Input = document.getElementById("tryRow1Letter5Input");
@@ -37,32 +54,34 @@ var answerLetter3Input = document.getElementById("answerLetter3Input");
 var answerLetter4Input = document.getElementById("answerLetter4Input");
 var answerLetter5Input = document.getElementById("answerLetter5Input");
 var submitAttempt = document.getElementById("submitAttempt");
+
 var row1 = [tryRow1Letter1Input, tryRow1Letter2Input, tryRow1Letter3Input, tryRow1Letter4Input, tryRow1Letter5Input];
+var row1id = ["tryRow1Letter1Input", "tryRow1Letter2Input", "tryRow1Letter3Input", "tryRow1Letter4Input", "tryRow1Letter5Input"];
 var row2 = [tryRow2Letter1Input, tryRow2Letter2Input, tryRow2Letter3Input, tryRow2Letter4Input, tryRow2Letter5Input];
+var row2id = ["tryRow2Letter1Input", "tryRow2Letter2Input", "tryRow2Letter3Input", "tryRow2Letter4Input", "tryRow2Letter5Input"];
 var row3 = [tryRow3Letter1Input, tryRow3Letter2Input, tryRow3Letter3Input, tryRow3Letter4Input, tryRow3Letter5Input];
+var row3id = ["tryRow3Letter1Input", "tryRow3Letter2Input", "tryRow3Letter3Input", "tryRow3Letter4Input", "tryRow3Letter5Input"];
 var row4 = [tryRow4Letter1Input, tryRow4Letter2Input, tryRow4Letter3Input, tryRow4Letter4Input, tryRow4Letter5Input];
+var row4id = ["tryRow4Letter1Input", "tryRow4Letter2Input", "tryRow4Letter3Input", "tryRow4Letter4Input", "tryRow4Letter5Input"];
 var row5 = [tryRow5Letter1Input, tryRow5Letter2Input, tryRow5Letter3Input, tryRow5Letter4Input, tryRow5Letter5Input];
+var row5id = ["tryRow5Letter1Input", "tryRow5Letter2Input", "tryRow5Letter3Input", "tryRow5Letter4Input", "tryRow5Letter5Input"];
+
 var rows = [row1, row2, row3, row4, row5];
-usingRowNumber = 0;
-for (var i = 0; i <= 4; i++) {
-	rows[i][0].placeholder = chosenWord[0];
-}
-var usingRowNumber; //i can change this by using event.target
-table.onkeydown = function (e) {
+var rowsid = [row1id, row2id, row3id, row4id, row5id];
+var usingRowNumber = 0; //i can change this by using event.target
+
+table.onkeyup = function (e) {
 	var target = e.srcElement || e.target;
 	var maxLength = target.maxLength;
 	var myLength = target.value.length;
-	if (event.keyCode === 8) {
-		rows[usingRowNumber].indexOf(target).value = "";
-	}
-	if (myLength >= maxLength && event.keyCode !== 8 || event.keyCode === 39) {
-		var next = rows[usingRowNumber].indexOf(target);
+	if (myLength >= maxLength) {
+		var next = rowsid[usingRowNumber].indexOf(target.id);
 		next++;
 		if (next < 5) {
 			rows[usingRowNumber][next].select();
 		}
-	} else if (myLength === 0 || event.keyCode === 37) {
-		var previous = rows[usingRowNumber].indexOf(target);
+	} else if (myLength === 0) {
+		var previous = rowsid[usingRowNumber].indexOf(target.id);
 		previous--;
 		if (previous > 0) {
 			rows[usingRowNumber][previous].select();
@@ -71,23 +90,27 @@ table.onkeydown = function (e) {
 	if (event.keyCode === 13 && rows[usingRowNumber][0].value.length >= 1 && rows[usingRowNumber][1].value.length >= 1 && rows[usingRowNumber][2].value.length >= 1 && rows[usingRowNumber][3].value.length >= 1 && rows[usingRowNumber][4].value.length >= 1) {
 		document.getElementById('submitAttempt').click();
 	}
-	//	if (target === rows[usingRowNumber][0]) {
-	//		rows[usingRowNumber][0].value = chosenWord.charAt(0);
-	//	}
+	if (target === rows[usingRowNumber][0]) {
+		rows[usingRowNumber][0].value = eLetter1;
+		
+	}
 };
-for (var i = 0; i <= 4; i++) {
-	rows[i][0].value = chosenWord.charAt(0);
-}
+
+tryRow1Letter1Input.value = eLetter1;
+tryRow2Letter1Input.value = eLetter1;
+tryRow3Letter1Input.value = eLetter1;
+tryRow4Letter1Input.value = eLetter1;
+tryRow5Letter1Input.value = eLetter1;
+
 submitAttempt.onclick = function () {
 	combineAttemptLettersAndCompareWithChosenWord();
 };
+
 function combineAttemptLettersAndCompareWithChosenWord() {
-	attempt = "";
-	for (var i = 0; i <= 4; i++) {
-		attempt += rows[usingRowNumber][i].value;
-	}
-	if (attempt.toLowerCase() === chosenWord) {
-		for (i = 0; i <= 4; i++) {
+	attempt = eLetter1 + CryptoJS.AES.encrypt(rows[usingRowNumber][1].value.toLowerCase(), key, { iv: iv }) + CryptoJS.AES.encrypt(rows[usingRowNumber][2].value.toLowerCase(), key, { iv: iv }) + CryptoJS.AES.encrypt(rows[usingRowNumber][3].value.toLowerCase(), key, { iv: iv }) + CryptoJS.AES.encrypt(rows[usingRowNumber][4].value.toLowerCase(), key, { iv: iv });
+	
+	if (attempt === eChosenWord) {
+		for (var i = 0; i <= 4; i++) {
 			rows[usingRowNumber][i].style.backgroundColor = "#fb5454";
 		}
 		answerLetter1Input.value = chosenWord[0];
@@ -101,68 +124,24 @@ function combineAttemptLettersAndCompareWithChosenWord() {
 		comparSplitWordsRow();
 	}
 }
-var repeatedLettersCount = 0;
+
 function comparSplitWordsRow() {
 	for (var i = 0; i <= 4; i++) {
 		if (splitAttempt[i].toLowerCase() === splitChosenWord[i]) {
 			rows[usingRowNumber][i].style.backgroundColor = "#fb5454";
 			rows[usingRowNumber][i].disabled = true;
-			var n = rows[usingRowNumber].indexOf(rows[usingRowNumber][i].parentElement.previousElementSibling.firstElementChild);
-			if (repeatedLettersCount < 0) {
-				for (; n > 0; n--) {
-					if (rows[usingRowNumber][n].value === splitAttempt[i]) {
-						rows[usingRowNumber][n].style.backgroundColor = "#becfd6";
-						rows[usingRowNumber][n].style.borderRadius = "5px";
-					}
-				}
-				repeatedLettersCount--;
-			}
 		} else if (splitChosenWord.includes(splitAttempt[i].toLowerCase())) {
-			for (var k = 0; k <= 4; k++) {
-				if (chosenWord.split(chosenWord[k]).length >= 3) {
-					//console.log(chosenWord[k]);
-					repeatedLettersCount = chosenWord.split(chosenWord[k]).length - 1;
-				}
-			}
-			var x = rows[usingRowNumber].indexOf(rows[usingRowNumber][i].parentElement.previousElementSibling.firstElementChild);
-			if (repeatedLettersCount > 0) {
-				rows[usingRowNumber][i].style.backgroundColor = "#cece2a";
-				rows[usingRowNumber][i].style.borderRadius = "100%";
-				rows[usingRowNumber][i].disabled = true;
-				if (rows[usingRowNumber][x].value === rows[usingRowNumber][i].value) {
-						//console.log(rows[usingRowNumber][i].value + " - multiple letters detected");
-						rows[usingRowNumber][i].style.backgroundColor = "#cece2a";
-						rows[usingRowNumber][i].style.borderRadius = "100%";
-						rows[usingRowNumber][i].disabled = true;
-						break;
-					}
-			} else {
-				for (; x >= 0; x--) {
-					if (rows[usingRowNumber][x].value === rows[usingRowNumber][i].value) {
-						//console.log(rows[usingRowNumber][i].value + " - multiple letters detected");
-						rows[usingRowNumber][i].style.backgroundColor = "#becfd6";
-						rows[usingRowNumber][i].style.borderRadius = "5px";
-						rows[usingRowNumber][i].disabled = true;
-						break;
-					} else if (splitChosenWord.includes(splitAttempt[i].toLowerCase())) {
-						//console.log(rows[usingRowNumber][i].value + " - no multiple letters detected");
-						rows[usingRowNumber][i].style.backgroundColor = "#cece2a";
-						rows[usingRowNumber][i].style.borderRadius = "100%";
-						rows[usingRowNumber][i].disabled = true;
-					}
-				}
-			}
-			repeatedLettersCount--;
-
+			rows[usingRowNumber][i].style.backgroundColor = "#cece2a";
+			rows[usingRowNumber][i].style.borderRadius = "100%";
+			rows[usingRowNumber][i].disabled = true;
 		} else {
 			rows[usingRowNumber][i].style.backgroundColor = "#becfd6";
-			rows[usingRowNumber][i].style.borderRadius = "5px";
 			rows[usingRowNumber][i].disabled = true;
 		}
-
 	}
 	startNewRow();
 }
+
 function startNewRow() {
 	usingRowNumber++;
 	if (usingRowNumber === 5) {
@@ -180,3 +159,60 @@ function startNewRow() {
 	}
 
 }
+
+
+
+
+////////////OLD VERSION
+
+//tryRow1Letter1Input.onkeyup = function() {
+//	if (this.value.length >= 1) {
+//		tryRow1Letter2Input.select();
+//	}
+//	tryRow1Letter1Input.value = chosenWord.charAt(0);
+//};
+//
+//tryRow1Letter2Input.onkeyup = function() {
+//	if (this.value.length >= 1) {
+//		tryRow1Letter3Input.select();
+//	}
+//	if (this.value.length === 0) {
+//		tryRow1Letter1Input.focus();
+//	}
+//	if (event.keyCode === 13 && rows[usingRowNumber][0].value.length >= 1 && rows[usingRowNumber][1].value.length >= 1 && rows[usingRowNumber][2].value.length >= 1 && rows[usingRowNumber][3].value.length >= 1 && rows[usingRowNumber][4].value.length >= 1) {
+//		document.getElementById('submitAttempt').click();
+//	}
+//};
+//
+//tryRow1Letter3Input.onkeyup = function() {
+//	if (this.value.length >= 1) {
+//		tryRow1Letter4Input.select();
+//	}
+//	if (this.value.length === 0) {
+//		tryRow1Letter2Input.select();
+//	}
+//	if (event.keyCode === 13 && rows[usingRowNumber][0].value.length >= 1 && rows[usingRowNumber][1].value.length >= 1 && rows[usingRowNumber][2].value.length >= 1 && rows[usingRowNumber][3].value.length >= 1 && rows[usingRowNumber][4].value.length >= 1) {
+//		document.getElementById('submitAttempt').click();
+//	}
+//};
+//
+//tryRow1Letter4Input.onkeyup = function() {
+//	if (this.value.length >= 1) {
+//		tryRow1Letter5Input.select();
+//	}
+//	if (this.value.length === 0) {
+//		tryRow1Letter3Input.select();
+//	}
+//	if (event.keyCode === 13 && rows[usingRowNumber][0].value.length >= 1 && rows[usingRowNumber][1].value.length >= 1 && rows[usingRowNumber][2].value.length >= 1 && rows[usingRowNumber][3].value.length >= 1 && rows[usingRowNumber][4].value.length >= 1) {
+//		document.getElementById('submitAttempt').click();
+//	}
+//};
+//
+//tryRow1Letter5Input.onkeyup = function() {
+//	if (this.value.length === 0) {
+//		tryRow1Letter4Input.select();
+//	}
+//	if (event.keyCode === 13 && rows[usingRowNumber][0].value.length >= 1 && rows[usingRowNumber][1].value.length >= 1 && rows[usingRowNumber][2].value.length >= 1 && rows[usingRowNumber][3].value.length >= 1 && rows[usingRowNumber][4].value.length >= 1) {
+//		document.getElementById('submitAttempt').click();
+//	}
+//};
